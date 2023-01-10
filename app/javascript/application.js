8892f97e054dca4bd48621d87db4be6260f07994
yasinabdmahmood <abd181103@gmail.com>
2023-01-10 18:11:19 +0300

    Modify  app/javascript/application.js to hold the react app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,21 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 // import "./controllers"
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-function App() {
-  return (<h1>Hello World!!!!</h1>);
-}
 
 ReactDOM.render(
-  <App/>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
